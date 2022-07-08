60db20d516c1ba42dbc7a7e6eac13150f9e367ab
Louis Benagha <80588773+ArerepadeBenagha@users.noreply.github.com>
2022-07-08 14:36:21 -0500

    Rhel (#17)
    * adding redhat vm to test ansible
    
    * adding redhat vm to test ansible
    
    * adding redhat vm to test ansible

--- servers/linux-server.tf
+++ servers/linux-server.tf
@@ -1,51 +1,51 @@
-resource "azurerm_network_interface" "labnic" {
-  name                = join("-", [local.server, "lab", "nic"])
-  location            = local.buildregion
-  resource_group_name = azurerm_resource_group.elite_general_resources.name
+# resource "azurerm_network_interface" "labnic" {
+#   name                = join("-", [local.server, "lab", "nic"])
+#   location            = local.buildregion
+#   resource_group_name = azurerm_resource_group.elite_general_resources.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.backendnetwork.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.labpip.id
-  }
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = data.azurerm_subnet.backendnetwork.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.labpip.id
+#   }
+# }
 
-resource "azurerm_public_ip" "labpip" {
-  name                = join("-", [local.server, "lab", "pip"])
-  resource_group_name = azurerm_resource_group.elite_general_resources.name
-  location            = local.buildregion
-  allocation_method   = "Static"
+# resource "azurerm_public_ip" "labpip" {
+#   name                = join("-", [local.server, "lab", "pip"])
+#   resource_group_name = azurerm_resource_group.elite_general_resources.name
+#   location            = local.buildregion
+#   allocation_method   = "Static"
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
 
-resource "azurerm_linux_virtual_machine" "Linuxvm" {
-  name                = join("-", [local.server, "linux", "vm"])
-  resource_group_name = azurerm_resource_group.elite_general_resources.name
-  location            = local.buildregion
-  size                = "Standard_DS1"
-  admin_username      = "adminuser"
-  user_data           = data.cloudinit_config.userdata.rendered
-  network_interface_ids = [
-    azurerm_network_interface.labnic.id,
-  ]
+# resource "azurerm_linux_virtual_machine" "Linuxvm" {
+#   name                = join("-", [local.server, "linux", "vm"])
+#   resource_group_name = azurerm_resource_group.elite_general_resources.name
+#   location            = local.buildregion
+#   size                = "Standard_DS1"
+#   admin_username      = "adminuser"
+#   user_data           = data.cloudinit_config.userdata.rendered
+#   network_interface_ids = [
+#     azurerm_network_interface.labnic.id,
+#   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file(var.path_to_publickey)
-  }
+#   admin_ssh_key {
+#     username   = "adminuser"
+#     public_key = file(var.path_to_publickey)
+#   }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    offer     = "0001-com-ubuntu-server-focal"
-    publisher = "Canonical"
-    sku       = "20_04-lts-gen2"
-    version   = "latest"
-  }
-}
+#   source_image_reference {
+#     offer     = "0001-com-ubuntu-server-focal"
+#     publisher = "Canonical"
+#     sku       = "20_04-lts-gen2"
+#     version   = "latest"
+#   }
+# }
